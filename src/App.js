f92952a0bab940a3f78a354d6b65d574b66cadeb
SShipon <shamsulislamshipon2022@gmail.com>
2021-09-26 04:54:48 +0600

    this

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Headear from './componenet/Headear/Header.js';
+import Header from './componenet/Headear/Header.js';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+    
+   
+  );
+}
+
+export default App;
---
+++ src/componenet/Headear/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-session">
+            <h4>Make in web developre team Bangladesh</h4>
+            <p>Explore our structured learning paths to discover everything you need to know about building for the modern web.</p>
+            <h1><span className="header-total">Total Budget</span> :120 Mililon</h1>
+        </div>
+    );
+};
+
+export default Header;
